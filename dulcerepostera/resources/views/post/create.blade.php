3f09f961d638b8623d1917c793369ca215f91f36
Santiago_Albisser <santiagoalbisser@hotmail.com>
2020-09-06 12:52:47 -0500

    Creación de la clase Post. subiendola master

---
+++ dulcerepostera/resources/views/post/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.master')
+
+@section("title", $data["title"])
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @include('util.message')
+            <div class="card">
+                <div class="card-header">Create post</div>
+                <div class="card-body">
+                    @if($errors->any())
+                    <ul id="errors">
+                        @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                    @endif
+                    <form method="POST" action="{{ route('post.save') }}">
+                        @csrf
+                        <input type="text" placeholder="Enter name" name="name" value="{{ old('name') }}" />
+                        <input type="text" placeholder="description" name="description" value="{{ old('description') }}" />
+                        <input type="submit" value="Send" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- dulcerepostera/routes/web.php
+++ dulcerepostera/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/index', 'HomeController@index')->name("home.index");
+Route::get('/post/show', 'PostController@show')->name("post.show");
+Route::get('/post/create', 'PostController@create')->name("post.create");
+Route::post('/post/save', 'PostController@save')->name("post.save");
+Route::get('/post/showpost/{id}', 'PostController@showpost')->name("post.showpost");
+Route::post('/post/delete/{id}', 'PostController@delete')->name("post.delete");
